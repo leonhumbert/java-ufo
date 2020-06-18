e42222fdff732464eef9be326cfe31cb9a6f168e
Umberto Leone <leon.fukasawa@gmail.com>
2020-06-18 11:12:56 -0700

    setting most to es6

--- UFOCode/static/js/app.js
+++ UFOCode/static/js/app.js
@@ -1,39 +1,41 @@
-// Variables for d3
-var button = d3.select("#filter-btn");
-var inputField1 = d3.select("#datetime");
-var inputField2 = d3.select("#city");
-var tbody = d3.select("tbody");
-var resetbtn = d3.select("#reset-btn");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
-
-var populate = (dataInput) => {
+// Follow pseudocode flow!
+// Variables for d3 
+let button = d3.select("#filter-btn");
+let inputField1 = d3.select("#datetime");
+let inputField2 = d3.select("#city");
+let tbody = d3.select("tbody");
+let resetbtn = d3.select("#reset-btn");
+let columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+//for using to populate table
+let populate = (dataInput) => {
 
   dataInput.forEach(ufo_sightings => {
-    var row = tbody.append("tr");
+    let row = tbody.append("tr");
     columns.forEach(column => row.append("td").text(ufo_sightings[column])
     )
   });
 }
 
-//Populate table
+//Populate table with data
 populate(data);
 
 // Filter by attribute
 button.on("click", () => {
   d3.event.preventDefault();
-  var inputDate = inputField1.property("value").trim();
-  var inputCity = inputField2.property("value").toLowerCase().trim();
+  let inputDate = inputField1.property("value").trim();
+  let inputCity = inputField2.property("value").toLowerCase().trim();
   // Filter by field matching input value
-  var filterDate = data.filter(data => data.datetime === inputDate);
+
+  let filterDate = data.filter(data => data.datetime === inputDate);
   console.log(filterDate)
-  var filterCity = data.filter(data => data.city === inputCity);
+  let filterCity = data.filter(data => data.city === inputCity);
   console.log(filterCity)
-  var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
+  let filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
   console.log(filterData)
 
   // Add filtered sighting to table
   tbody.html("");
-
+  //ES6
   let response = {
     filterData, filterCity, filterDate
   }
